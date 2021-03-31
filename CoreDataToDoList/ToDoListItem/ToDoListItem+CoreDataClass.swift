5d3865e9c224f3451c07d90723d90bea4e580214
Sophie <sophienanterre@gmail.com>
2021-03-31 11:40:01 +0200

    CoreDataToDoList avec tableView

---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDoList
+//
+//  Created by Sophie Jacquot  on 31/03/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
