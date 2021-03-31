4c006635bb6df2ed2b8d950debaac1bb8571f195
Sophie <sophienanterre@gmail.com>
2021-03-31 10:39:35 +0200

    Initial Commit

---
+++ CoreDataToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataToDoList
+//
+//  Created by Sophie Jacquot  on 31/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
